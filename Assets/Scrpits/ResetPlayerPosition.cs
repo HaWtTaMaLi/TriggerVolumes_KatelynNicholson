8ae5f28c7a7497096609c6cf22e466ee46830d48
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-21 11:41:51 -0300

    Scripts
    setting scripts and scene

---
+++ Assets/Scrpits/ResetPlayerPosition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ResetPlayerPosition : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //set player position back to start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
