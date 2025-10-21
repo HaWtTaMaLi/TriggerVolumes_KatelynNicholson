db99429803b623f24542429a9a88e48980ce87c0
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-21 15:59:16 -0300

    push

--- Assets/Scrpits/LightsOnOff.cs
+++ Assets/Scrpits/LightsOnOff.cs
@@ -2,17 +2,10 @@ using UnityEngine;
 
 public class LightsOnOff : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject Light;
-    void Start()
-    {
-        //trigger RGB ligts
-        //stays on when leave but turnes off when enter again
-    }
+    public Animator lightAnimator;
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider other)
     {
-        
+        lightAnimator.SetBool("RGBRainbow", true);
     }
 }
